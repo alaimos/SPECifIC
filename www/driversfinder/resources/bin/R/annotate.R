0bb500273a4468e4d005fe2ccaf98fe469c3a51b
Salvatore Alaimo <alaimos@gmail.com>
2016-12-30 22:15:41 +0100

    Working on enrichment scripts and database

---
+++ www/driversfinder/resources/bin/R/annotate.R
@@ -0,0 +1,61 @@
+#!/usr/bin/env Rscript
+##########################################################################################################
+# Developed by S. Alaimo (alaimos at dmi dot unict dot it)
+##########################################################################################################
+script.dir <- dirname((function() {
+    cmdArgs <- commandArgs(trailingOnly = FALSE)
+    needle <- "--file="
+    match <- grep(needle, cmdArgs)
+    if (length(match) > 0) {
+        # Rscript
+        return(normalizePath(sub(needle, "", cmdArgs[match])))
+    } else {
+        # 'source'd via R console
+        return(normalizePath(sys.frames()[[1]]$ofile))
+    }
+})())
+source(paste0(script.dir, "/setup.R"))
+require.packages(c("getopt"))
+source(paste0(script.dir, "/common.R"))
+load(paste0(script.dir, "/../../data/enrichment_data.RData"))
+
+cmd.line.valid.args <- matrix(c(
+    "list",               "l",  1,  "character",  "A file containing a list of genes in a subpathway",
+    "maxpv",              "p",  2,  "character",  "Maximal p-value threshold",
+    "pvadjust",           "a",  2,  "character",  "A pvalue adjustment method",
+    "output",             "o",  1,  "character",  "Output",
+    "help",               "h",  0,  "logical",    "This help"
+), ncol=5, byrow=TRUE)
+
+opt <- getopt(cmd.line.valid.args)
+
+if (!is.null(opt$help)) {
+    cat(paste(getopt(cmd.line.valid.args, usage=TRUE), "\n"))
+    q(status=100, save="no")
+} else {
+    tryCatch({
+        if (!file.exists(opt$list)) {
+            stop("Input list file does not exist")
+        }
+        if (is.null(opt$pvadjust)) {
+            opt$pvadjust <- "BH"
+        }
+        if (is.null(opt$maxpv)) {
+            opt$maxpv <- 1
+        }
+        lst <- unique(unlist(strsplit(x=readLines(opt$list), split=",", fixed=TRUE)))
+        er  <- compute.enrichment(gs=lst, enrichment.data=enrichment.data, 
+                                  total.annotated=total.annotated, 
+                                  total.annotated.by.term=total.annotated.by.term,
+                                  enrichment.map=enrichment.map, 
+                                  p.adjust.method=opt$pvadjust)
+        result <- Reduce(rbind, er)
+        result <- result[result$p.value.adjusted <= opt$maxpv,]
+        write.table(result, file=opt$output, append=FALSE, quote=TRUE, sep="\t", row.names=FALSE, 
+                    col.names=TRUE)
+    }, error=function (e) {
+        cat(e$message,"\n")
+        q(status=102, save="no")
+    })
+    q(status=0, save="no")
+}
--- www/driversfinder/resources/bin/R/common.R
+++ www/driversfinder/resources/bin/R/common.R
@@ -1,4 +1,3 @@
-#!/usr/bin/env Rscript
 ##########################################################################################################
 # Developed by S. Alaimo (alaimos at dmi dot unict dot it)
 ##########################################################################################################
@@ -25,3 +24,76 @@ make.heatmap <- function (case, cntl, selection, output) {
         unlink("Rplots.pdf")
     }
 }
+
+compute.pvalue <- function (gs, terms, total, total.by.terms, terms.map=NULL, p.adjust.method="BH") {
+    annot <- intersect(gs, names(terms))
+    if (length(annot) <= 0) {
+        return (NULL)
+    }
+    selected.terms <- unique(unname(unlist(terms[annot])))
+    N <- total
+    n <- length(annot)
+    M <- total.by.terms[selected.terms]
+    k <- numeric(length(selected.terms))
+    names(k) <- selected.terms
+    for (g in annot) {
+        k[terms[[g]]] <- k[terms[[g]]] + 1
+    }
+    p <- numeric(length(selected.terms))
+    names(p) <- selected.terms
+    for (t in selected.terms) {
+        #p[t] <- 1 - phyper(q=k[t],m=M[t],n=N-M[t],k=n)
+        p[t] <- phyper(q=k[t],m=M[t],n=N-M[t],k=n, lower.tail = FALSE)
+    }
+    selected.terms.name <- selected.terms
+    if (!all(is.null(terms.map))) {
+        selected.terms.name <- terms.map[selected.terms]
+    }
+    rho <- M/N
+    result <- data.frame(
+        term.id=selected.terms,
+        term.name=selected.terms.name,
+        term.count=k,
+        term.expected=n*rho,
+        term.var=((n*rho*(1-rho)*(N-n))/(N-1)),
+        p.value=p,
+        p.value.adjusted=p.adjust(p, method=p.adjust.method),
+        ts.M=M,
+        ts.N=N,
+        ts.n=n
+    )
+    result <- result[order(result$p.value.adjusted),]
+    return (result)
+}
+
+compute.enrichment <- function (gs, enrichment.data, total.annotated, total.annotated.by.term, 
+                                enrichment.map, p.adjust.method="BH", prev="") {
+    result <- vector("list", length(0))
+    #names(result) <- names(enrichment.data)
+    for (n in names(enrichment.data)) {
+        nxt <- ifelse(prev=="",n,paste0(prev,".",n))
+        if (is.list(enrichment.data[[n]][[1]])) {
+            result <- c(result, compute.enrichment(gs=gs, 
+                                                   enrichment.data=enrichment.data[[n]], 
+                                                   total.annotated=total.annotated[[n]], 
+                                                   total.annotated.by.term=total.annotated.by.term[[n]], 
+                                                   enrichment.map=enrichment.map[[n]], 
+                                                   p.adjust.method=p.adjust.method, prev=nxt))
+        } else {
+            tmp <- compute.pvalue(gs=gs, 
+                                  terms=enrichment.data[[n]], 
+                                  total=total.annotated[[n]], 
+                                  total.by.terms=total.annotated.by.term[[n]], 
+                                  terms.map=enrichment.map[[n]], 
+                                  p.adjust.method=p.adjust.method)
+            if (!all(is.null(tmp))) {
+                tmp$enrichment.type <- nxt
+                result <- c(result, list(tmp))
+            }
+        }
+    }
+    return (result)
+}
+
+
+
